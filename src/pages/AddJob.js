401caa0758d9049d2baa8bd8feb526f80534bccc
CoderRohu <rohangouda348@gmail.com>
2026-03-13 02:20:37 +0530

    Add input fields for Company and Role in AddJob

--- src/pages/AddJob.js
+++ src/pages/AddJob.js
@@ -1,8 +1,26 @@
 function AddJob() {
+
   return (
-    <div>
-      <h2>Add Job Application</h2>
-      <p>Job form coming soon</p>
+    <div style={{marginTop:"30px"}}>
+
+      <h2>Add Job</h2>
+
+      <input placeholder="Company"
+      style={{padding:"10px",margin:"5px"}}/>
+
+      <input placeholder="Role"
+      style={{padding:"10px",margin:"5px"}}/>
+
+      <button style={{
+        padding:"10px 20px",
+        background:"#2563eb",
+        color:"white",
+        border:"none",
+        borderRadius:"5px"
+      }}>
+        Add
+      </button>
+
     </div>
   );
 }
