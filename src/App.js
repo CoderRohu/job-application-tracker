cd0c992ac032ce917cbd0b41eb3b81b322adefe8
CoderRohu <rohangouda348@gmail.com>
2026-03-13 02:18:59 +0530

    Improved main UI layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import Dashboard from "./components/Dashboard";
+import AddJob from "./pages/AddJob";
+
 function App() {
   return (
-    <div>
-      <h1>Job Application Tracker</h1>
-      <p>Track your job applications efficiently.</p>
+    <div style={{background:"#f4f6f8",minHeight:"100vh",padding:"20px"}}>
+      
+      <h1 style={{textAlign:"center",color:"#1e3a8a"}}>
+        Job Application Tracker
+      </h1>
+
+      <Dashboard/>
+
+      <AddJob/>
+
     </div>
   );
 }
